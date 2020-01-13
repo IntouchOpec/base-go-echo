2fb22fe76288a0c3b4cfac7a6853623c8ff7ecd2
Intouch Teeramartwanit <intouch.opec@gmail.com>
2020-01-13 19:22:25 +0700

    no message

--- router/channel/router.go
+++ router/channel/router.go
@@ -1,9 +1,6 @@
 package channel
 
 import (
-	"fmt"
-	"html/template"
-	"io"
 	"net/http"
 
 	. "github.com/IntouchOpec/base-go-echo/conf"
@@ -14,39 +11,39 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
-type Template struct {
-	templates *template.Template
-}
+// type Template struct {
+// 	templates *template.Template
+// }
 
-type BaseTempleRespone struct {
-	Title       string `json:"title"`
-	AccountName string `json:"account_name"`
-}
+// type BaseTempleRespone struct {
+// 	Title       string `json:"title"`
+// 	AccountName string `json:"account_name"`
+// }
 
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	if viewContext, isMap := data.(echo.Map); isMap {
-		csrfValue := c.Get("_csrf")
-		viewContext["base"] = echo.Map{"title": fmt.Sprintf("%s", viewContext["title"])}
-		viewContext["_csrf"] = csrfValue
-	}
-	return t.templates.ExecuteTemplate(w, name, data)
-}
+// func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+// 	if viewContext, isMap := data.(echo.Map); isMap {
+// 		csrfValue := c.Get("_csrf")
+// 		viewContext["base"] = echo.Map{"title": fmt.Sprintf("%s", viewContext["title"])}
+// 		viewContext["_csrf"] = csrfValue
+// 	}
+// 	return t.templates.ExecuteTemplate(w, name, data)
+// }
 
 // Routers channel.
 func Routers() *echo.Echo {
 	e := echo.New()
 
-	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-		ContextKey:  "_csrf",
-		TokenLookup: "form:_csrf",
-	}))
+	// e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+	// 	ContextKey:  "_csrf",
+	// 	TokenLookup: "form:_csrf",
+	// }))
 
-	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
-	}
+	// t := &Template{
+	// 	templates: template.Must(template.ParseGlob("public/views/*.html")),
+	// }
 	e.Use(cache.Cache())
 
-	e.Renderer = t
+	// e.Renderer = t
 
 	e.GET("/", indexHandler)
 
